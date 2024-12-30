968620d1876e279b22137913804b84d90fd0941d
Ismael3s <souz4ismael@gmail.com>
2024-12-30 07:42:01 -0300

    feat: added missing cancellation token

--- src/Dotnet.Cep.Infra/Implementations/CepsGateways/RefitBrasilApiCepGateway.cs
+++ src/Dotnet.Cep.Infra/Implementations/CepsGateways/RefitBrasilApiCepGateway.cs
@@ -6,14 +6,14 @@ namespace Dotnet.Cep.Infra.Implementations.CepsGateways;
 public interface IRefitBrazilApiCepGateway
 {
     [Get("/api/cep/v2/{cep}")]
-    public Task<CepResponse> Find(string cep);
+    public Task<CepResponse> Find(string cep, CancellationToken cancellationToken = default);
 }
 
 public sealed class BrasilApiCepGateway(IRefitBrazilApiCepGateway httpClient) : ICepGateway
 {
     public async Task<CepResponse> Find(string cep, CancellationToken cancellationToken = default)
     {
-        var response = await httpClient.Find(cep);
+        var response = await httpClient.Find(cep, cancellationToken);
         return response with
         {
             Gateway = "BrasilApi"
--- src/Dotnet.Cep.Infra/Implementations/CepsGateways/ViaCepGateway.cs
+++ src/Dotnet.Cep.Infra/Implementations/CepsGateways/ViaCepGateway.cs
@@ -6,7 +6,7 @@ namespace Dotnet.Cep.Infra.Implementations.CepsGateways;
 public interface IRefitViaCepGateway
 {
     [Get("/ws/{cep}/json")]
-    Task<ViaCepResponse> Find(string cep);
+    Task<ViaCepResponse> Find(string cep, CancellationToken cancellationToken = default);
 }
 
 public sealed record ViaCepResponse(string Logradouro, string Bairro, string Localidade, string Estado);
@@ -15,7 +15,7 @@ public sealed class ViaCepGateway(IRefitViaCepGateway httpClient) : ICepGateway
 {
     public async Task<CepResponse> Find(string cep, CancellationToken ct = default)
     {
-        var response = await httpClient.Find(cep);
+        var response = await httpClient.Find(cep, cancellationToken: ct);
         return new CepResponse(cep, response.Estado, response.Bairro, City: response.Localidade,
             Street: response.Logradouro, Gateway: "ViaCep");
     }
